449037c201ece0afca5aa306906110d2d95b05cc
staz6 <muhammad.aahad81@gmail.com>
2023-05-23 16:05:02 +0500

    Addid add friend code

--- lib/bloc/repository.dart
+++ lib/bloc/repository.dart
@@ -1,20 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 class Repository {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<void> addFriend(String name, String email, String mobileNo) {
     print("friend add.");
-    return _firestore.collection('friends').add({
+    return FirebaseFirestore.instance.collection('friends').add({
       'name': name,
       'email': email,
       'mobileNo': mobileNo,
-    });
+    }).then((value) => print(value)).catchError((error)=>print(error));
   }
 
   Future<void> editFriend(
       String friendId, String name, String email, String mobileNo) {
-    return _firestore.collection('friends').doc(friendId).update({
+    return FirebaseFirestore.instance.collection('friends').doc(friendId).update({
       'name': name,
       'email': email,
       'mobileNo': mobileNo,
